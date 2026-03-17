c7a863f130057c7345428596d121217720dbc6a7
Morgan Mednick <m.g.mednick@wustl.edu>
2026-03-17 10:39:36 -0500

    Added transaction logic to BankAccount

--- src/main/BankAccount.java
+++ src/main/BankAccount.java
@@ -3,14 +3,17 @@ package main;
 public class BankAccount {
 
     private double balance;
+    private Transactions transactions;
 
     public BankAccount() {
         this.balance = 0;
+        this.transactions = new Transactions();
     }
 
     public void deposit(double amount) {
         if(amount > 0) {
             this.balance += amount;
+            transactions.addTransaction(new Transaction(amount, "Deposit", "Depositing " + amount));
         } else {
             throw new IllegalArgumentException();
         }
