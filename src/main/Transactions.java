79befb9333d9fc394dc7a80e62c8c64da0210bf6
Morgan Mednick <m.g.mednick@wustl.edu>
2026-03-17 10:35:55 -0500

    Added transactions and wrapper

--- src/main/Transactions.java
+++ src/main/Transactions.java
@@ -1,7 +1,42 @@
 package main;
 
-public class Transactions {
-    //ArrayList of Transactions
-}
+import java.util.ArrayList;
+import java.util.List;
 
-//DisplayTransactions: Prints a list of them that looks good
+public class Transactions {
+
+    private List<Transaction> transactions;
+
+    public Transactions() {
+        transactions = new ArrayList<>();
+    }
+
+    public void addTransaction(Transaction t) {
+        transactions.add(t);
+    }
+
+    public List<Transaction> getAllTransactions() {
+        return transactions;
+    }
+
+    // Display nicely
+    public void displayTransactions() {
+        if (transactions.isEmpty()) {
+            System.out.println("No transactions found.");
+            return;
+        }
+
+        System.out.println("---- Transaction History ----");
+
+        for (int i = 0; i < transactions.size(); i++) {
+            Transaction t = transactions.get(i);
+
+            System.out.println(
+                (i + 1) + ". " +
+                t.getType() +
+                " | $" + t.getAmount() +
+                " | " + t.getDescription()
+            );
+        }
+    }
+}
