ac4504e2384b79e3fee18dde3f4a3cc11d02ecb5
Christopher Barber <barber.c@wustl.edu>
2026-04-08 08:54:29 -0500

    Made ViewAllAccount's output clearer/more user friendly

--- src/main/Menus/MenuOptions/ViewAllBalancesOption.java
+++ src/main/Menus/MenuOptions/ViewAllBalancesOption.java
@@ -5,6 +5,7 @@ import main.AppContext;
 import main.BankAccount;
 import main.BankAccounts;
 import main.Users.User;
+import main.Users.UserRole;
 import main.Users.UserService;
 
 public class ViewAllBalancesOption implements IMenuOption {
@@ -17,16 +18,23 @@ public class ViewAllBalancesOption implements IMenuOption {
         BankAccounts allAccounts = ctx.getAllAccounts();
         if(allAccounts.getNumAccounts() == 0){
             System.out.println("There are no active accounts right now.");
+            return;
         }
 
         for(User user : UserService.getUsers().values()){
-            System.out.println(user.getUsername());
+            if(user.getRole() == UserRole.Administrator){
+                continue;
+            }
+            System.out.println("Customer: " + user.getUsername());
+            if(user.getAccountIds().size() == 0){
+                System.out.println("    No active accounts");
+            }
             for(int id : user.getAccountIds()){
                 BankAccount account = allAccounts.getAccount(id);
-                System.out.println("Account #" + account.getId() + ": $" + account.getBalance());
+                System.out.println("    Account #" + account.getId() + ": $" + account.getBalance());
                 //TODO: Once we implement different account types, it may be useful to include that information in this string too
-                System.out.println();
             }
+            System.out.println();
         }
     }
 }
